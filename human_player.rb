21319f0b8bb4704459e849acab1d3108fc73edbd
Knolle-03 <Knolle-03@users.noreply.github.com>
2018-11-15 11:42:15 +0100

    Added generate_code for human and ai

--- human_player.rb
+++ human_player.rb
@@ -9,7 +9,23 @@ require_relative 'ui'
 class HumanPlayer
 
 
-  def generate_code(valid_values, expected_length)
-
+  def generate_code(valid_values, code_length)
+  loop do
+    wrong_symbols = []
+    code = UI.get_code_array(valid_values, code_length)
+    unless code.length == code_length
+      UI.display_wrong_length(code_length, code.length)
+    unless code.each_index { |symbol| valid_values.include?(symbol) }
+      code.each_index do |symbol|
+        unless valid_values.include?(symbol)
+          wrong_symbols << symbol
+        end
+      UI.display_wrong_symbols(wrong_symbols)
+        end
+    end
+    UI.display_wrong_symbols
+    break if code.each_index { |i| valid_values.include?(i) } && code.length == code_length
+    end
+  end
   end
 end
