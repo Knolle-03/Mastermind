17e0352a0e8ec825de0f0072e2d8699a72fb4e2b
Knolle-03 <Knolle-03@users.noreply.github.com>
2018-11-27 23:38:29 +0100

    reworked mastermind_calc

--- scriptt.rb
+++ scriptt.rb
@@ -1,30 +1,57 @@
-guess = %w[1 2 3 6]
 code = %w[1 2 3 2]
-blacks = 0
-guess.each_with_index { |value, i| blacks += 1 if value == code[i] }
+guess = %w[4 2 2 3]
 
-# 'whites = 0
-# tested_values = []
-# guess.each do |value|
-#   tested_values << value
-#   c = code.count(value)
-#   whites += c
-# end'
 
+
+
+guess2 = guess.dup
+code2 = code.dup
 whites = 0
-guess_uniq = guess.uniq
-guess.uniq.each do |value|
-  c = code.count(value) -
-      whites += c
-
+blacks = code2.zip(guess2).find_all { |a, b| a == b }.count
+guess2.each do |value|
+  if code2.include?(value)
+    code2.delete_at(code2.index(value))
+    puts code2.to_s
+    whites += 1
+  end
+  whites
 end
-whites = whites - blacks
-puts blacks
-puts whites
+hits = [blacks, whites - blacks]
 
+
+puts code2.to_s
+puts guess2.to_s
+puts '--------------------'
+puts hits[0]
+puts hits[1]
+
+
+
+
+
+
+
+
+
+
+
+#puts blacks.to_s
+#puts code.to_s
+
+# whites = 0
+# guess_uniq = guess.uniq
+# guess.uniq.each do |value|
+#   c = code.count(value) -
+#       whites += c
+#
+# end
+# whites = whites - blacks
+# puts blacks
+# puts whites
+=begin
 puts %w[1 2 3 2].uniq
 
-
+=end
 
 
 
